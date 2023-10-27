b0b1301df22907b5acf890f03f6c8ec5699a0cc7
Tom ツ <50146042+realt0w@users.noreply.github.com>
2023-10-27 02:03:36 +0200

    Added news section
    Added complete news section.

---
+++ articles.js
@@ -0,0 +1,10 @@
+const articles = [
+    {
+        title: "Bakugan Project - Klepto's Early Access Analysis",
+        description: "This document is meant to highlight all of the important gameplay aspects shown and/or mentioned in Klepto’s Early Access Streams on 10/23-10/25.",
+        date: "27-10-2023",
+        image: "./images/news/1.jpg",
+        path: "./news/1.html",
+    },
+    // Ajoutez d'autres articles ici...
+];
